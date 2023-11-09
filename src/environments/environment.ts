a0743c2e5afd997e951b70a4d9b0b3e6cc5fbe25
Mihajlo Gluhovic <mihajlogluhovic@gmail.com>
2023-11-09 18:58:25 +0100

    Weather App
    Weather app made using Angular

---
+++ src/environments/environment.ts
@@ -0,0 +1,8 @@
+export const environment = {
+  production: false,
+  weatherApiBaseUrl: 'https://open-weather13.p.rapidapi.com',
+  XRapidAPIHostHeaderName: 'X-RapidAPI-Host',
+  XRapidAPIHostHeaderValue: 'open-weather13.p.rapidapi.com',
+  XRapidAPIKeyHeaderName: 'X-RapidAPI-Key',
+  XRapidAPIKeyHeaderValue: '47057936femshd082fda9a2c1b0bp118174jsn3ae5ecf962f6',
+};
